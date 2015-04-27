0443da417c7573ce1b874e60a5a11b0f20148567
1n@n@j <jusjanani@gmail.com>
2015-04-28 03:17:55 +0530

    Update 1a.py

--- 1a.py
+++ 1a.py
@@ -1,5 +1,6 @@
 #!/usr/local/env python
+import re
 
-pdb_fn = raw_input("Enter PDB filename [test1/pdb]: ") or "test1.pdb"
-
+pdb_fn = raw_input("Enter PDB filename [test_ensemble.pdb]: ") or "test_ensemble.pdb"
 fi = open(pdb_fn,"r")
+ca_lines = [l for l in fi.readlines() if re.match("ATOM.* CA ",l)] 
